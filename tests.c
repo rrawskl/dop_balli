1317562e2177b2aa247592cb260d80d24ef3277f
rrawskl <tvanisimov08@gmail.com>
2026-09-17 11:35:16 +0300

    add solution

--- tests.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("%d\n", get_kopecks(10, 50, 3));
-    printf("%d\n", get_kopecks(5, 25, 4));
-    printf("%d\n", get_kopecks(7, 99, 2));
-
-    return 0;
-}
