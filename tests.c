47412a60317d0909d33ea1472ad4369979d5c301
rrawskl <tvanisimov08@gmail.com>
2026-09-17 11:18:07 +0300

    add tests

---
+++ tests.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("%d\n", get_kopecks(10, 50, 3));
+    printf("%d\n", get_kopecks(5, 25, 4));
+    printf("%d\n", get_kopecks(7, 99, 2));
+
+    return 0;
+}
