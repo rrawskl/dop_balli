145463719c1c1352316733ce817aee08fa7362eb
Karim <alberkucev@gmail.com>
2026-09-17 12:31:29 +0300

    add solution

--- task2.c
+++ task2.c
@@ -1,3 +1,12 @@
-//
-// Created by Karim on 17.09.2026.
-//
+#include <stdio.h>
+
+int check_div(int k, int n) {
+    return k % n;
+}
+
+int main() {
+    printf("%d\n", check_div(10, 3));
+    printf("%d\n", check_div(10, 5));
+    printf("%d\n", check_div(11, 4));
+    return 0;
+}
