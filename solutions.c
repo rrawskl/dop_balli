ca65c4780d110fd70670402906b37f2a001d7ce0
rrawskl <tvanisimov08@gmail.com>
2026-09-21 10:42:41 +0300

    Add task2 to solutions

--- solutions.c
+++ solutions.c
@@ -14,3 +14,15 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+int check_div(int k, int n) {
+    return k % n;
+}
+
+int main() {
+    printf("%d\n", check_div(10, 3));
+    printf("%d\n", check_div(10, 5));
+    printf("%d\n", check_div(11, 4));
+    return 0;
+}
